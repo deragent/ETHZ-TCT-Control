91323af64867497e6fc9a3cb8ecca90df11e8b36
Johannes Wüthrich <jwuethri@phys.ethz.ch>
2022-04-20 07:03:41 +0200

    Uses waveform integran in Trial_Scan_XY

--- trial/Trial_Scan_XY.py
+++ trial/Trial_Scan_XY.py
@@ -1,6 +1,13 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors
+
+plt.rcParams.update({
+    'figure.figsize': (12.0, 8.0),
+    'font.size': 16,
+})
+
 
 from tct.system import Setup
 from tct.logger import Logger
@@ -9,10 +16,10 @@ log = Logger(print=True, debug=False)
 
 s = Setup(vlimit=1000, ilimit=0.002, log=log)
 
-Z = 73.133 # mm
+Z = 78.2 # mm
 
-Xs = np.linspace(0, 2, 21)
-Ys = np.linspace(-2.5, -0.5, 21)
+Xs = np.linspace(-2.1, -0.9, 7)
+Ys = np.linspace(0.3, 1.5, 7)
 
 data = np.zeros((len(Xs), len(Ys)))
 
@@ -20,7 +27,6 @@ s.scope.scope.TriggerMode(s.scope.scope.TRIGGER_MODE.NORMAL)
 s.scope.scope.ClearSweeps('C2')
 s.scope.scope.Average('C2', 100)
 time.sleep(0.1)
-s.scope.scope.GetAverage('C2')
 
 s.laser.LaserSetFrequency(50e3)
 s.laser.LaserSetDAC(290)
@@ -31,6 +37,7 @@ s.amp.AmpOn()
 
 s.bias.SMURampVoltage(120)
 s.bias.SMUOn()
+print(f'Current: {s.bias.SMUCurrent()*1e6:.2f} uA')
 
 for xx, X in enumerate(Xs):
     for yy, Y in enumerate(Ys):
@@ -39,10 +46,17 @@ for xx, X in enumerate(Xs):
 
         wave = s.scope.AcquireAverage()
 
-        data[xx, yy] = np.max(wave.y)
+        # # Maximum Amplitude
+        # data[xx, yy] = np.max(wave.y)
+
+        # Integral of pulse
+        sel = wave.x >= 0
+        offset = np.mean(wave.y[wave.x < 0])
+        amplitude = wave.y[sel] - offset
+        data[xx, yy] = np.trapz(amplitude, wave.x[sel])
+
 
         print(f'{X}\t{Y}\t{Z}\t{data[xx, yy]}')
-
 
 s.Off()
 
