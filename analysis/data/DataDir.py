0b98f55535b8e8ccbd8c99957f13e78ba3f860b1
Johannes Wüthrich <jwuethri@phys.ethz.ch>
2022-07-05 18:17:54 +0200

    Adds analysis datadir representation

---
+++ analysis/data/DataDir.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pandas as pd
+
+from .Scan import Scan
+
+class DataDir():
+
+    def __init__(self, folder):
+
+        self.folder = Path(folder)
+        if not self.folder.is_dir():
+            raise Exception(f'Data directory [{self.folder}] does not exist.')
+
+        # self.global_log = self.folder / 'log.txt'
+
+        self._info = None
+        self._scans = {}
+        self.reload()
+
+    def _loadScan(self, path):
+        dataset = path.name
+
+        scan = Scan(path)
+        info = scan.info()
+
+        return dataset, info, scan
+
+    def reload(self):
+        info = {}
+        self._scans = {}
+
+        for path in self.folder.iterdir():
+            if path.is_dir():
+                try:
+                    dataset, meta, scan = self._loadScan(path)
+
+                    info[dataset] = meta
+                    self._scans[dataset] = scan
+                except Exception as e:
+                    print(e)
+                    pass
+
+        self._info = pd.DataFrame.from_dict(info, orient='index')
+
+    def scans(self):
+        return self._info
+
+    def scan(self, dataset):
+        if dataset in self._scans:
+            return self._scans[dataset]
+
+        return None
--- analysis/data/__init__.py
+++ analysis/data/__init__.py
@@ -1,1 +1,2 @@
 from .Scan import Scan
+from .DataDir import DataDir
