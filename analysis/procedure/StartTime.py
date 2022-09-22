cb5b4b00b875ad4a293fbcd580f5931dc43012cb
Johannes Wüthrich <jwuethri@phys.ethz.ch>
2022-09-22 16:24:59 +0200

    Adds debug plot to gradient start time fit

--- analysis/procedure/StartTime.py
+++ analysis/procedure/StartTime.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from ..preprocess import generic
 
@@ -28,8 +29,7 @@ class StartTimeGradient():
         self._threshold = threshold
         self._rise = rise
 
-    def fit(self, time, amplitude):
-        peak = np.max(amplitude)
+    def fit(self, time, amplitude, debug=False):
 
         amplitude_filtered =  generic.MovingAvg(amplitude, Navg=40)
         gradient = np.gradient(amplitude_filtered)
@@ -37,13 +37,43 @@ class StartTimeGradient():
 
         start_idx = np.argmax(gradient_filtered)
 
-        if peak >= self._threshold:
+        if np.max(amplitude) >= self._threshold:
             fit_sel = range(start_idx-3*self._rise, start_idx+1*self._rise)
             pfit = np.polyfit(time[fit_sel], amplitude[fit_sel], 1)
 
-            return -1*pfit[1]/pfit[0]
+            start = -1*pfit[1]/pfit[0]
         else:
-            return float('nan')
+            start = float('nan')
+
+        if debug:
+            return start, (amplitude_filtered, gradient_filtered, start_idx, fit_sel, pfit)
+        else:
+            return start
+
+    def plot(self, time, amplitude):
+        fig, (ax0, ax1) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]}, sharex = True)
+
+        start, debug = self.fit(time, amplitude, debug=True)
+
+        ax0.axhline(0, color='k', alpha=0.3)
+
+        lines = ax0.plot(time, amplitude, label='TCT Pulse')
+        ax0.plot(time, debug[0], color=lines[0].get_color(), linestyle='--', label='Filtered Pulse')
+
+        time_fit = time[debug[3]]
+        ax0.axvspan(time_fit[0], time_fit[-1], color='k', alpha=0.15, label='Fit Range')
+
+        time_plot = np.linspace(start, time_fit[-1], 50)
+        ax0.plot(time_plot, np.polyval(debug[4], time_plot), 'k-.', label='Linear Fit')
+
+        ax1.plot(time, debug[1], 'g', label='Filtered Gradient')
+        ax0.axvline(time[debug[2]], linestyle='--', color='k')
+        ax1.axvline(time[debug[2]], linestyle='--', color='k', label='Maximum Gradient')
+
+        ax0.plot(start, 0, color='r', marker='o', linestyle=None, label=f'Start Time')
+
+        return fig, ax0, ax1
+
 
 
 class MedianStartTime():
