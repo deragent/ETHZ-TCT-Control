4a34d48f7f2a4508fe9bce7e9228beca0744ccf6
Johannes Wüthrich <jwuethri@phys.ethz.ch>
2022-12-09 15:54:04 +0100

    Adds Bragg-like charge distribution for alpha simulation

--- analysis/simulation/signal/util/charge.py
+++ analysis/simulation/signal/util/charge.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.stats
 
+import pybragg
+
 from ....physics import constants as pc
 
 def point(Neh, position, e=True, h=True):
@@ -39,6 +41,37 @@ def exponential(Neh, position, lbda, Nlbda=5, points=26, e=True, h=True):
 
     charges = []
     for x, val in zip(charge_range, exp_pdf):
+        if e:
+            charges.append((x, -pc.e0*val))
+        if h:
+            charges.append((x, pc.e0*val))
+
+    return charges
+
+def alpha(Neh, pos, surface=-503e-6, Npoints=31, e=True, h=True):
+    ## pos is the approximate peak location
+    ## surface is the location of the silicon wafer surface
+
+    # Based on interaction of Am-241 5.485MeV alpha in Si
+    # Based on measurements on the 30.10.2022 (with ca. 9mm air gap)
+    # -> Nominal pos ~ -483e-6 / Nominal Neh ~ 1.28e6
+
+    # Constants (fitted to TRIM simulation)
+    SIGMA = 8.7e-7
+    P = 1.415
+    K = 0.002
+
+    peak = pos - surface
+    depth = np.linspace(0, peak, Npoints)
+    dose_pdf = pybragg.bortfeld(depth, D100=1000, R0=peak+SIGMA, sigma=SIGMA, p=P, k=K)
+
+    # Normalize dose to total Neh
+    dose_pdf = Neh*dose_pdf/np.sum(dose_pdf)
+
+    charge_range = np.linspace(surface, pos, Npoints)
+
+    charges = []
+    for x, val in zip(charge_range, dose_pdf):
         if e:
             charges.append((x, -pc.e0*val))
         if h:
